4b2356c66fb34522a2b4a9584e6947192b287d7a
Curtis H <ch@curtish.me>
2016-07-12 21:00:03 +0100

    limiting title sizes

--- resources/js/controllers/main.js
+++ resources/js/controllers/main.js
@@ -2,10 +2,16 @@
  * the main controller for the site
  * handle getting the items from flickr and showing in the view
  */
-app.controller('mainController', ['$scope', 'FlickrService', function($scope, Flickr) {
+app.controller('mainController', ['$scope', '$window', 'FlickrService', function($scope, $window, Flickr) {
     // initialise blank array
     $scope.feed = [];
+    // setup and watch the window size to base our title limits
+    angular.element($window).bind('load resize', function() {
+        $scope.limiter = Math.floor($window.innerWidth / 20);
+        $scope.$apply();
+    });
 
+    
     // get the data from flickr and add to the scope
     Flickr.getData().then(function(data) {
         $scope.feed = data;
