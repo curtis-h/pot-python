18d8da6ee00ee2bd423c38044b7d1bdc15c0c26a
Curtis H <ch@curtish.me>
2016-07-12 22:23:13 +0100

    adding in load more functionality and button

--- resources/js/controllers/main.js
+++ resources/js/controllers/main.js
@@ -4,15 +4,25 @@
  */
 app.controller('mainController', ['$scope', '$window', 'FlickrService', function($scope, $window, Flickr) {
     // initialise blank array
-    $scope.feed = [];
+    $scope.feed    = [];
+    // hide the load more button while loading
+    $scope.loading = true;
+    
     // setup and watch the window size to base our title limits
     angular.element($window).bind('load resize', function() {
         $scope.limiter = Math.floor($window.innerWidth / 20);
         $scope.$apply();
     });
     
-    // get the data from flickr and add to the scope
-    Flickr.getData().then(function(data) {
-        $scope.feed = data;
-    });
+    // contain the logic to add flickr items to the feed
+    $scope.addToFeed = function() {
+        $scope.loading = true;
+        // get the data from flickr and add to the scope
+        Flickr.getData().then(function(data) {
+            $scope.feed    = data;
+            $scope.loading = false;
+        });
+    };
+    // run once on instantiation
+    $scope.addToFeed();
 }]);
